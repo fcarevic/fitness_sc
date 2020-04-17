385ce35ba4d53fb8e00bda2761978380e3770938
Filip Carevic <filip.carevic11@gmail.com>
2020-04-18 00:17:19 +0200

    pocetna strana izgled odradjen

---
+++ js/skripta.js
@@ -0,0 +1,18 @@
+var niz= [
+    '<br> <br><br><br><p class="text-muted mb-0 ">Ukoliko ste u duši Spartanac ili Spartanka – ovaj trening je idealan za vas.</p><br> <br><br>',
+    '<br> <br><br><br><p class="text-muted mb-0 ">Oslobadnjanje od stresa za lepse polovine.</p><br> <br><br>',
+    '<br> <br><br><br><p class="text-muted mb-0 ">Sve u sluzbi vaseg zdravlja.</p><br> <br><br>'
+                   ];
+var niz2 = [' <br> <br><br><br><br><br> <br><br><h3 class="h4 mb-2 crna rounded">Crossfit</h3>',
+' <br> <br><br><br><br><br> <br><br><h3 class="h4 mb-2 crna rounded">Pilates</h3>',
+' <br> <br><br><br><br><br> <br><br><h3 class="h4 mb-2 crna rounded">Spinning</h3>'
+];
+
+function prikaziTekst(id, enter){
+    return;
+    elem=document.getElementById(id);
+    let br = parseInt(elem.id);
+    if(enter)
+    elem.innerHTML =  niz[br];
+    else elem.innerHTML =  niz2[br];
+}
