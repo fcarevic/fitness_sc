94cd1376f9311d329f59049f6ca045f92e9ce518
Filip Carevic <filip.carevic11@gmail.com>
2020-05-02 01:53:38 +0200

    gotovo sortiranje i izgled konkretnog treninga

---
+++ js/pregledKategorije.js
@@ -0,0 +1,8 @@
+var opisi = new Map();
+opisi.put('klasicni',  'Savremeni način života (sedenje za kompjuterom, fizička neaktivnost) donosi bolove u leđima i različite probleme sa kičmom i zglobovima. Pilates koji vežbamo danas evoluirao je iz rekreativnog u terapeutski program vežbanja. Veliki broj školi Pilatesa usmerio je svoje programe i istraživanja ka ovom obliku Pilatesa, pa danas nije retko da fizioterapeuti savetuju svoje pacijente da vežbaju Pilates.',
+);
+
+$(document).ready(function(){
+  
+
+});
--- js/pregledTreninga.js
+++ js/pregledTreninga.js
@@ -56,10 +56,77 @@ $(document).ready(function(){
             $(".treningCol").collapse('hide');
           }
       });
+///////////////////////////////////////
+$("li img").click(function() {
+    switch(this.id){
+        case "naziv":  sortByName(); break;
+        case "trajanje"  : sortByDuration(); break;
+        case "tezina": sortByQuality(); break;
+    }
+    
+});
+
+
+      update();
 
 
 
 
 
 
-})
+});
+
+var baza = [
+{ 
+    "naslov" : "Klasični pilates",
+    "opis": " Pilates je inovativni i siguran mind&body program. Vežbajući Pilates razvijate svesnost o svom telu, dobru posturu i lake i graciozne pokrete.",
+    "slika":"../assets/slike/pilates/klasicniPilates-1.jpg",
+    "trajanje":45,
+    "tezina":3
+
+},
+{ 
+    "naslov" : "Reformer pilates",
+    "opis": " Sam trening je dinamičniji, zabavniji i delotvorniji a rezultati evidentniji u dalekom kraćem vremenu nego kod pilatesa na podlozi. Na naprednom vežbačkom nivou, u jednom Reforemer času postiže se i efekat sagorevanja viška masnoće paralelno sa izduživanjem i učvršćivanjem mišića. Istovremeno, svaka Reformer vežba po prirodi i isteže mišiće koje vežba.",
+    "slika":"../assets/slike/pilates/reformerPilates.jpg",
+    "trajanje":60,
+    "tezina":4
+},
+{ 
+    "naslov" : "Stott pilates",
+    "opis": "      Uz pomoć STOTT PILATESA očuvaćete gipkost, izdužiti mišiće i što je još važnije imaćete pravilno držanje tela, jer ćete ojačati mišiće koji čine potporu kičmenog stuba.",
+    "slika":"../assets/slike/pilates/stottPilates.jpg",
+    "trajanje":30,
+    "tezina":5
+},
+
+];
+
+function sortByName(){
+    baza.sort(function(a,b) { return a.naslov>b.naslov? 1 : ((a.naslov<b.naslov)?-1: 0) });
+    update();
+}
+function sortByQuality() {
+    baza.sort(function(a,b) { return a.tezina-b.tezina});
+    update();
+}
+function sortByDuration() {
+    baza.sort(function(a,b) { return a.trajanje-b.trajanje});
+    update();
+}
+function update(){
+    let naslovi= document.getElementsByClassName("card-header");
+    let opisi =   document.getElementsByClassName("card-text");
+    let slike =  document.getElementsByClassName("card-img-top");
+    let tezine = document.getElementsByClassName("tezina");
+    let trajanja = document.getElementsByClassName("trajanje");
+ for(let i=0 ; i< naslovi.length;i++){
+     naslovi[i].innerHTML=baza[i].naslov;
+    opisi[i].innerHTML=baza[i].opis;
+    slike[i].src=baza[i].slika;
+    tezine[i].innerHTML=baza[i].tezina;
+    trajanja[i].innerHTML=baza[i].trajanje;
+    }
+
+
+}
